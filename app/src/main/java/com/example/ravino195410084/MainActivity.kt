e29d4cfd9c09f2a455d8cddcebc89b08ba0302e8
195410084-AchmadRavino <achmadravino2111@gmail.com>
2021-10-22 22:54:32 +0700

    pertemuan6

---
+++ app/src/main/java/com/example/ravino195410084/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ravino195410084
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
